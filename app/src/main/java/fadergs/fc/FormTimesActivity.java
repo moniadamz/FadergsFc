0cc55d2bc2e5e9f96c3cde1793d2ff03f43bd363
Vinicius Bonilha <vmbonilha@gmail.com>
2019-09-20 13:50:17 -0300

    Criando form de times

---
+++ app/src/main/java/fadergs/fc/FormTimesActivity.java
@@ -0,0 +1,13 @@
+package fadergs.fc;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FormTimesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_times);
+    }
+}
---
+++ app/src/main/java/fadergs/fc/ListaTimesActivity.java
@@ -0,0 +1,14 @@
+package fadergs.fc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaTimesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_times);
+    }
+}
