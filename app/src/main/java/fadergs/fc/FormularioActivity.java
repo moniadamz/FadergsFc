ccdc4d52fdc820f13080f97906dc8c9a8778dfbd
Vinicius Bonilha <vmbonilha@gmail.com>
2019-09-22 13:52:27 -0300

    Editando times e preenchendo hogadores

--- app/src/main/java/fadergs/fc/FormularioActivity.java
+++ app/src/main/java/fadergs/fc/FormularioActivity.java
@@ -6,19 +6,31 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class FormularioActivity extends AppCompatActivity {
 
     private Button activityFormJogadoresBtSalvar;
-
+    private EditText nomeTime;
+    private EditText nomeJogador;
+    private EditText camisa;
+    TimeDAO dao = new TimeDAO();
+    private Time time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_jogadores);
 
-        activityFormJogadoresBtSalvar = findViewById(R.id.activityFormJogadoresBtSalvar);
 
+        nomeTime = findViewById(R.id.activityFormJogadoresTime);
+
+        Intent dadosTime = getIntent();
+        Time time  = (Time) dadosTime.getSerializableExtra("time");
+        nomeTime.setText(time.getNome());
+
+/*
+        activityFormJogadoresBtSalvar = findViewById(R.id.activityFormJogadoresBtSalvar);
         activityFormJogadoresBtSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,5 +38,7 @@ public class FormularioActivity extends AppCompatActivity {
                 startActivity( i );
             }
         });
+*/
+
     }
 }
--- app/src/main/java/fadergs/fc/ListaTimesActivity.java
+++ app/src/main/java/fadergs/fc/ListaTimesActivity.java
@@ -8,16 +8,21 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.List;
 
 public class ListaTimesActivity extends AppCompatActivity {
 
+    public static final String TITULO_APPBAR = "Lista de Times";
+    private TimeDAO dao = new TimeDAO();
     private ListView lvTimes;
 
     @Override
@@ -25,28 +30,59 @@ public class ListaTimesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_times);
 
-        TimeDAO dao = new TimeDAO();
-        List<Time> times = dao.getTimes(this);
-        //dao.close();
+        setTitle(TITULO_APPBAR);
 
-        ListView listaTimes = (ListView) findViewById(R.id.ActivityListaTimes);
-        ArrayAdapter<Time> adapter = new ArrayAdapter<Time>(this, android.R.layout.simple_list_item_1, times);
-        listaTimes.setAdapter(adapter);
+        configuraFabNovoTime();
+    }
 
-        setTitle("Lista de Times");
-
-
+    private void configuraFabNovoTime() {
         FloatingActionButton fab = findViewById(R.id.floatingActionButton);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i = new Intent(ListaTimesActivity.this, FormTimesActivity.class);
-                startActivity( i );
+                abreFormTimeActivity();
+            }
+        });
+    }
+
+    private void abreFormTimeActivity() {
+        startActivity( new Intent(this, FormTimesActivity.class) );
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        configuraLista();
+    }
+
+    @NotNull
+    private void configuraLista() {
+        ListView listaTimes = findViewById(R.id.ActivityListaTimes);
+        final List<Time> times = dao.getTimes(this);
+        listaTimes.setAdapter( new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, times));
+
+        listaTimes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int posicao, long id) {
+                Time timeClicado = times.get(posicao);
+                Intent formJogadores = new Intent(ListaTimesActivity.this, FormularioActivity.class);
+                formJogadores.putExtra("time", timeClicado);
+                startActivity(formJogadores);
+
+                Log.i("lista", "onItemClick:"+ timeClicado);
+            }
+        });
+
+        listaTimes.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int posicao, long id) {
+                excluir((Time) adapterView.getItemAtPosition(posicao));
+                return true;
             }
         });
 
     }
-
 
     private void excluir(final Time time){
         AlertDialog.Builder alerta = new AlertDialog.Builder(this);
@@ -72,20 +108,12 @@ public class ListaTimesActivity extends AppCompatActivity {
 
     private void carregarLista(){
         List<Time> lista = TimeDAO.getTimes(this);
+
+        ListView listaTimes = findViewById(R.id.ActivityListaTimes);
+        ArrayAdapter<Time> adapter = new ArrayAdapter<Time>(this, android.R.layout.simple_list_item_1, dao.getTimes(this));
+
+        listaTimes.setAdapter(adapter);
         Log.i("Carregar lista", "carregarLista: " + lista);
-
-/*        if ( lista.size() == 0 ){
-            lvTimes.setEnabled( false );
-            Time fake = new Time();
-            fake.setNome("Lista Vazia!");
-            lista.add( fake );
-        }else {
-            lvTimes.setEnabled( true );
-        }*/
-/*
-        AdapterTime adapter = new AdapterTime(this, lista);
-
-        lvTimes.setAdapter( adapter );*/
 
     }
 }
